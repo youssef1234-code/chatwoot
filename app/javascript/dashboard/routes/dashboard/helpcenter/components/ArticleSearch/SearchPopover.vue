cb91f6938146e955f8cf9d9b4390a94ce73019f7
Youssef Mohamed Mohamed <58553094+youssef1234-code@users.noreply.github.com>
2024-09-05 01:17:43 +0300

    Update SearchPopover.vue
    Trial for fixing pagination issue in import

--- app/javascript/dashboard/routes/dashboard/helpcenter/components/ArticleSearch/SearchPopover.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/components/ArticleSearch/SearchPopover.vue
@@ -27,8 +27,11 @@ export default {
     return {
       searchQuery: '',
       isLoading: false,
+      isLoadingMore: false,
       searchResults: [],
       activeId: '',
+      currentPage: 1,
+      totalPages: 1,
       debounceSearch: () => {},
     };
   },
@@ -58,6 +61,7 @@ export default {
   mounted() {
     this.fetchArticlesByQuery(this.searchQuery);
     this.debounceSearch = debounce(this.fetchArticlesByQuery, 500, false);
+    this.setupScrollListener();
   },
   methods: {
     generateArticleUrl(article) {
@@ -74,6 +78,8 @@ export default {
     onSearch(query) {
       this.searchQuery = query;
       this.activeId = '';
+      this.currentPage = 1;
+      this.searchResults = [];
       this.debounceSearch(query);
     },
     onClose() {
@@ -81,23 +87,38 @@ export default {
       this.searchQuery = '';
       this.activeId = '';
       this.searchResults = [];
+      this.currentPage = 1;
     },
-    async fetchArticlesByQuery(query) {
+    async fetchArticlesByQuery(query, append = false) {
+      if (this.currentPage > this.totalPages) return; // Stop if no more pages
+
       try {
         const sort = query ? '' : 'views';
-        this.isLoading = true;
-        this.searchResults = [];
+        this.isLoading = !append;
+        this.isLoadingMore = append;
+
         const { data } = await ArticlesAPI.searchArticles({
           portalSlug: this.selectedPortalSlug,
           query,
-          sort,
+          page: this.currentPage, // Pass the current page
         });
-        this.searchResults = data.payload;
-        this.isLoading = true;
+
+        if (append) {
+          this.searchResults = [...this.searchResults, ...data.payload];
+        } else {
+          this.searchResults = data.payload;
+        }
+
+        this.totalPages = data.meta.total_pages; // Update total pages
+
+        if (append) {
+          this.currentPage++;
+        }
       } catch (error) {
-        // Show something wrong message
+        // Handle error appropriately
       } finally {
         this.isLoading = false;
+        this.isLoadingMore = false;
       }
     },
     handlePreview(id) {
@@ -112,6 +133,19 @@ export default {
       this.$emit('insert', article);
       useAlert(this.$t('HELP_CENTER.ARTICLE_SEARCH.SUCCESS_ARTICLE_INSERTED'));
       this.onClose();
+    },
+    setupScrollListener() {
+      const container = this.$el.querySelector('.search-results-container');
+      container.addEventListener('scroll', this.handleScroll);
+    },
+    handleScroll() {
+      const container = this.$el.querySelector('.search-results-container');
+      if (container.scrollHeight - container.scrollTop === container.clientHeight) {
+        // User has scrolled to the bottom
+        if (!this.isLoading && this.currentPage <= this.totalPages) {
+          this.fetchArticlesByQuery(this.searchQuery, true);
+        }
+      }
     },
   },
 };
@@ -138,15 +172,23 @@ export default {
         @back="onBack"
         @insert="onInsert"
       />
-      <SearchResults
-        v-else
-        :search-query="searchQuery"
-        :is-loading="isLoading"
-        :portal-slug="selectedPortalSlug"
-        :articles="searchResultsWithUrl"
-        @preview="handlePreview"
-        @insert="onInsert"
-      />
+      <div class="search-results-container overflow-y-auto" v-else>
+        <SearchResults
+          :search-query="searchQuery"
+          :is-loading="isLoading"
+          :portal-slug="selectedPortalSlug"
+          :articles="searchResultsWithUrl"
+          @preview="handlePreview"
+          @insert="onInsert"
+        />
+
+        <!-- Spinner for loading more articles -->
+        <div v-if="isLoadingMore" class="loading-more-spinner flex justify-center items-center py-4">
+          <div class="spinner"></div>
+          <span class="ml-2">Loading more articles...</span>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
